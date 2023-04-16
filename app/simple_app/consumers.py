a286fd561eb550d96ed64250a5049003e2f16836
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2023-04-16 03:40:28 +0300

    Use threading to synchronize time

--- app/simple_app/consumers.py
+++ app/simple_app/consumers.py
@@ -1,4 +1,8 @@
+import time
+
 from channels.generic.websocket import WebsocketConsumer
+from datetime import datetime
+import threading
 
 
 class EchoConsumer(WebsocketConsumer):
@@ -9,6 +13,12 @@ class EchoConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
         self.send(text_data='You are connected by web sockets!')
+
+        def send_current_time(self):
+            while True:
+                self.send(text_data=str(datetime.now().strftime('%H:%M:%S')))
+                time.sleep(1)
+        threading.Thread(target=send_current_time, args=(self,)).start()
 
     def disconnect(self, code):
         pass
